b088409bc54cb7493e6745dd0e18ef357f0fd82f
ArcherFsec <61965275+ArcherFsec@users.noreply.github.com>
2023-11-30 23:40:48 +0330

    chore: created auth service

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'dummy',
+      global: true,
+    }),
+  ],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService<
+  T extends Record<string, any> = Record<string, any>,
+  K extends Record<string, any> = T,
+> {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async getAccessToken(data: T): Promise<string> {
+    return this.jwtService.signAsync(data, {
+      algorithm: 'HS256',
+      expiresIn: '1h',
+    });
+  }
+
+  async getRefreshToken(data: K): Promise<string> {
+    return this.jwtService.signAsync(data, {
+      algorithm: 'HS512',
+      expiresIn: '4h',
+    });
+  }
+
+  async validate(data: string): Promise<T | K> {
+    return this.jwtService.verifyAsync(data, {
+      algorithms: ['HS512'],
+    });
+  }
+}
