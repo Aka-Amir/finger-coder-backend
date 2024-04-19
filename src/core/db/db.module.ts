a7d0a4cd76324607663e7606e7510f7cd60098ae
ArcherFsec <61965275+ArcherFsec@users.noreply.github.com>
2024-04-20 00:18:26 +0330

    chore: latest commit

--- src/core/db/db.module.ts
+++ src/core/db/db.module.ts
@@ -5,12 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mariadb',
+      type: process.env.NODE_ENV === 'DEV' ? 'mariadb' : 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_fingercoder',
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || 'root',
+      database: process.env.DB_NAME || 'db_fingercoder',
       autoLoadEntities: true,
       synchronize: true,
     }),
