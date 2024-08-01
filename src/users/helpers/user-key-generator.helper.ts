87e64072a621a41cddf7b9f0815e074d78623343
ArcherFsec <61965275+ArcherFsec@users.noreply.github.com>
2024-08-01 21:39:55 +0330

    chore: removed users module

--- src/users/helpers/user-key-generator.helper.ts
+++
@@ -1,5 +0,0 @@
-import { createHash } from 'crypto';
-
-export function userKeyGenerator(phoneNumber: string, code: string): string {
-  return createHash('MD5').update(`${phoneNumber}_${code}`).digest('hex');
-}
--- src/users/types/find.type.ts
+++
@@ -1,4 +0,0 @@
-export type FindType = {
-  fullName?: string;
-  phoneNumber?: string;
-};
