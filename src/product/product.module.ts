69753419ede889f47d8701e65f0515e24d159b4f
ArcherFsec <61965275+ArcherFsec@users.noreply.github.com>
2024-08-02 11:49:22 +0330

    chore: initiated tickets feature

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
 import { OfferCodesModule } from './offer-codes/offer-codes.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [EventsModule, OfferCodesModule],
+  imports: [EventsModule, OfferCodesModule, TicketsModule],
 })
 export class ProductModule {}
